264827625e3a44372895ba4fe795222ace232618
skdlfjl <ok951108@gmail.com>
2020-12-13 17:22:58 +0900

    투표에 참여하지 않은 학생들의 학번을 확인할 수 있는 액티비티 생성

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -14,18 +14,20 @@ public class MainActivity extends AppCompatActivity {
 
     static int find(String[] arr, String s) {
         for(int i = 0; i < arr.length; i++) {
-            if(arr[i].indexOf(s) >= 0)
-                return 1;
+            if(arr[i].equals(s)) {
+                break;
+            }
         }
-        return -1;
+        return 1;
     }
 
     private EditText et_token;
     private Button btn_insert;
+    private Button btn_search;
     private com.google.android.material.floatingactionbutton.FloatingActionButton btn_creat;
-    private Button btn_count;
+    //private Button btn_count;
     private String str;    // 입력된 토큰을 받는 변수입니다, 변수 명을 바꿔주면 좋을듯...
-    String[] token = {"1111","2222","3333","4444","5555"};   //이걸 데이터베이스에서 가져와야 합니다 (수정필요!!!!!!)
+    String[] token = {"1111","2222","3333","4444","5555"};   //이걸 데이터베이스에서 가져와야 합니다 (수정필요!!!!!!) token_dict의 tokens에서 가져옵니다
     int where;
 
     @Override
@@ -33,15 +35,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et_token = findViewById(R.id.et_token);
 
+
+        // 여기는 정말 코드가 많이 추가되어야 합니다.
+        // token_dict에서 일단 입력된 토큰(str변수에 저장했네요)을 key값으로 이용하게 됩니다.
+        // token_dict의 tokens를 사용하게 되는데, tokens[str] 값이 0이면 학번쓰는 페이지 sub로 넘어가고, 1이면 투표 완료페이지로 가야합니다.
+        // 물론 저장되지 않은 토큰이면 아래처럼 토스트메세지를 입력합니다
         btn_insert = findViewById(R.id.btn_insert);
         btn_insert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 버튼 클릭했을때 실행되는거!!
+                et_token = findViewById(R.id.et_token);
                 str = et_token.getText().toString();  // 넣은 토큰을 string형태로 가져와서 str(빈 변수)에 저장한다
-                where = find(token,str);
+                //where = find(token,str);
+                for(int i = 0; i < token.length; i++) {
+                    if(token[i].equals(str)) {
+                        where = 1;
+                        break;
+                    }
+                    where = 0;
+                }
                 if(where==1) {
                     Intent intent = new Intent(MainActivity.this, SubActivity.class);
                     intent.putExtra("str", str);  // putExtra로 str(토큰 들어있음)데이터를 담는다
@@ -62,12 +76,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btn_count = findViewById(R.id.btn_count);
-        btn_count.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+        // 위와 구현방법이 거의 동일합니다.
+        // 사용되는 토큰을 저장해둔 token_dict의 tokens의 토큰이 0일 경우, 페이지가 넘어갑니다.
+        // 1일경우 투표가 완료되었으므로 이미 투표가 완료되었다는 토스트 메세지를 출력합니다.
+        // 물론 저장되지 않은 토큰이면 아래처럼 토스트메세지를 입력합니다
+
+        btn_search = findViewById(R.id.btn_search);
+        btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, TestActivity.class);
-                startActivity(intent);
+                // 버튼 클릭했을때 실행되는거!!
+                et_token = findViewById(R.id.et_token);
+                str = et_token.getText().toString();  // 넣은 토큰을 string형태로 가져와서 str(빈 변수)에 저장한다
+                //where = find(token,str);
+                for(int i = 0; i < token.length; i++) {
+                    if(token[i].equals(str)) {
+                        where = 1;
+                        break;
+                    }
+                    where = 0;
+                }
+                if(where==1) {
+                    Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                    intent.putExtra("str", str);  // putExtra로 str(토큰 들어있음)데이터를 담는다
+                    startActivity(intent);  // 실제로 액티비티 이동하는 구문, 담은 데이터를 서브 액티비티로 쏜다!!  >>  서브 액티비티로 가서 코드를 만들어주자
+                }else{
+                    Toast.makeText(getApplicationContext(),"저장되어있지 않은 토큰입니다.",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
---
+++ app/src/main/java/com/example/test/SearchActivity.java
@@ -0,0 +1,64 @@
+package com.example.test;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SearchActivity extends AppCompatActivity {
+
+
+    private TextView tv_token;
+    private EditText et_search;
+    private Button btn_search;
+
+    private String search;
+
+
+
+    String[] student_id = {"20171478","20181234","20192345","20205532","20213356","33","33333","333333","1223123","233","1111111"};  //임시임
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        tv_token = findViewById(R.id.tv_token);
+        Intent intent = getIntent();  // 어디선가 날라오는 데이터값이 있으면 이쪽에서 그 값을 받겠다 = getIntent()
+        String str = intent.getStringExtra("str"); // 메인 액티비티에서 날려보낸 데이터값을 지정한 이름이 "str"이었으니까 여기도 "str"로 받는다
+        tv_token.setText(str);
+
+        // str변수에는 투표로 이동할 수 있는 token이 담겨있습니다.
+        // token_dict의 search를 사용해서, edittext에 입력된 관리자용 token이 맞는지 확인해야합니다.
+        // 대충 적어보면 search[str] == (edittext에 입력된 관리자용 token)
+        // 이걸 만족하면 T, 만족하지 않으면 관리자용 토큰이 틀린거니까 토스트 메세지를 출력합니다.
+        // T인경우에는
+        et_search = findViewById(R.id.et_search);
+        btn_search = findViewById(R.id.btn_search);
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search = et_search.getText().toString();
+                //여기서 원래 if문을 써야합니다링
+                //search[str] == (edittext에 입력된 관리자용 token)
+                //만약 T인 경우라면 dict2에서 value가 0인 사람들만 쭉 뽑아서 보여주면 됩니다
+                //일단 예시로 student_id를 사용합니다
+                String text = "";
+                for (int i = 0; i < student_id.length; i++) {
+                    text += student_id[i] + '\n';
+                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(SearchActivity.this);
+                builder.setTitle("투표 미참여 학번 명단").setMessage(text);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+            }
+        });
+
+    }
+}
