8c3155fe58612824eb19c0ed6d46bdb10431389f
Jimin <jjimini98@naver.com>
2020-12-13 20:48:36 +0900

    edit_dict.json , edit_search.txt , edit_token.txt have only 100 tokens

--- json/makeeditfile.py
+++ json/makeeditfile.py
@@ -2,7 +2,6 @@ import json
 from collections import OrderedDict
 
 
-
 dict1 = OrderedDict() #자동 정렬을 막아줌
 key =  OrderedDict()
 tokens = OrderedDict()
@@ -14,9 +13,8 @@ key["key"] = 0
 dict1["key"] = key
 
 
-
 #tokens.txt 파일에서 토큰을 읽어온다.
-with open("token_data/tokens.txt", "r") as f:
+with open("token_data/edit_token.txt", "r") as f:
     token= []
     line = f.readline()
     line = line.split(",")
@@ -26,37 +24,36 @@ with open("token_data/tokens.txt", "r") as f:
 
 number = 1
 
-edit_token = []
 for x in range(len(token[0])) :
         tokens[int(number)] = token[0][x]
-        if x <= 100:
-            edit_token.append(token[0][x])
         number +=1
 
-print(edit_token)
+
 #dict1 에 tokens 추가
 dict1["tokens"] = tokens
 
 
+
+
 #search.txt 파일에서 난수를 얻어온다.
-with open("token_data/search.txt", "r") as f:
+number2 =1
+
+with open("token_data/edit_search.txt", "r") as f:
     searches= []
     line = f.readline()
     line = line.split(",")
     while line!='':
         searches.append(line)
         line = f.readline()
-edit_search = []
+
 for x in range(len(searches[0])) :
-        search[int(number)] = searches[0][x]
-        if x <= 100:
-            edit_search.append(searches[0][x])
-        number +=1
-print(edit_search)
+        search[int(number2)] = searches[0][x]
+        number2 +=1
+
 # dict1에 search 추가
 dict1["search"] = search
 
 
-with open("dict_one.json", 'w') as make_file:
+with open("edit_dict.json", 'w') as make_file:
     json.dump(dict1, make_file, ensure_ascii=False, indent="")
 
